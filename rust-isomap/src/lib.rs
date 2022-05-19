facdcdb12f8423f2e8d4865754b23e05fa0ec48e
bsorrentino <bartolomeo.sorrentino@gmail.com>
2022-05-19 14:30:26 +0000

    feat(rust-isomap): use browser console and add node to DOM

--- rust-isomap/src/lib.rs
+++ rust-isomap/src/lib.rs
@@ -1,6 +1,7 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use web_sys::console;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -8,12 +9,36 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen]
+#[wasm_bindgen(js_namespace = window)]
 extern {
     fn alert(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet() {
-    alert("Hello, rust-module!");
+    let msg = "WASM ISOMAP";
+
+    alert( msg );
+
+    console::log_1( &msg.into() )
+
+
+}
+
+// Called by our JS entry point to run the example
+#[wasm_bindgen(start)]
+pub fn run() -> Result<(), JsValue> {
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    // Manufacture the element we're gonna append
+    let val = document.create_element("p")?;
+    val.set_text_content(Some("WASM ISOMAP"));
+
+    body.append_child(&val)?;
+
+    Ok(())
 }
