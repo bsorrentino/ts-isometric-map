0180d3dd1a3167734737bdc044cd69da1ca20b67
bsorrentino <bartolomeo.sorrentino@gmail.com>
2022-05-20 16:33:30 +0000

    test: use canvas element

--- rust-isomap/src/lib.rs
+++ rust-isomap/src/lib.rs
@@ -1,9 +1,15 @@
 mod utils;
+mod iso;
 
-use wasm_bindgen::prelude::*;
-use web_sys::console;
+use wasm_bindgen::{prelude::*, JsCast};
+use web_sys::{ 
+    console, 
+    Document, 
+    HtmlCanvasElement,
+};
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+
+// When t&he `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
@@ -28,17 +34,37 @@ pub fn greet() {
 // Called by our JS entry point to run the example
 #[wasm_bindgen(start)]
 pub fn run() -> Result<(), JsValue> {
+
+    utils::set_panic_hook();
+
     // Use `web_sys`'s global `window` function to get a handle on the global
     // window object.
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
     let body = document.body().expect("document should have a body");
 
+    let canvas = get_canvas(&document).unwrap_throw();
+
+    let txt_content = format!( "WASM ISOMAP : {}", &canvas.id() );
+
     // Manufacture the element we're gonna append
     let val = document.create_element("p")?;
-    val.set_text_content(Some("WASM ISOMAP"));
+    val.set_text_content(Some( &txt_content ));
 
     body.append_child(&val)?;
 
     Ok(())
+}
+
+
+fn get_canvas( document: &Document ) -> Result<HtmlCanvasElement, ()> {
+
+    let canvas_id = "canvas";
+
+    let expect_msg = format!("canvas id '{}' not found", canvas_id );
+    
+    let elem = document.get_element_by_id(canvas_id).expect( &expect_msg );
+
+    elem.dyn_into::<HtmlCanvasElement>().map_err(|_| ())
+
 }
